378152a610580baeead571cadfc469183a321d78
kivicode <31818467+kivicode@users.noreply.github.com>
2019-03-17 15:13:39 +0300

    Added accurate positioning + removed wheel class

--- Arduino/Small/Small.ino
+++ Arduino/Small/Small.ino
@@ -1,5 +1,17 @@
-#define Speed 100 // процентов
 
+#define Speed 100 // процентов
+#define LeftMotorBackwardPin 4
+#define LeftMotorForwardPin 3
+#define LeftMotorSpeedPin 2
+
+#define LeftEncoderPin 5
+
+
+#define RightMotorBackwardPin 9
+#define RightMotorForwardPin 8
+#define RightMotorSpeedPin 7
+
+#define RightEncoderPin 6
 /* TODO: Test me */
 typedef struct Command {
   String func;
@@ -8,22 +20,47 @@ typedef struct Command {
 
 void setup() {
   Serial.begin(9600);
-  forward(10);
-//  Serial.setTimeout(250);
+  pinMode(LeftMotorForwardPin, OUTPUT);
+  pinMode(LeftMotorBackwardPin, OUTPUT);
+
+  pinMode(RightMotorForwardPin, OUTPUT);
+  pinMode(RightMotorBackwardPin, OUTPUT);
+
+  pinMode(LeftMotorSpeedPin, OUTPUT);
+  pinMode(RightMotorSpeedPin, OUTPUT);
+
+  pinMode(LeftEncoderPin, INPUT_PULLUP);
+  pinMode(RightEncoderPin, INPUT_PULLUP);
+
+
+  Serial.setTimeout(150);
 }
 
+
+String SUCC = "~";
+
 void loop() {
-  //  while (Serial.available() > 0) {
-  //    Command cmd = readCommand();
-  //    String F = cmd.func;
-  //    if (F == "f") {
-  //      forward(cmd.param);
-  //    } else if (F == "b") {
-  //      backward(cmd.param);
-  //    } else if (F == "l") {
-  //      left(cmd.param);
-  //    } else if (F == "r") {
-  //      right(cmd.param);
-  //    }
-  //  }
+  while (Serial.available() > 0) {
+    String in = String(Serial.readString());
+    if (in != "") {
+      String cmd = split(in, '(', 0);
+      String params = split(split(in, '(', 1), ')', 0);
+      if (cmd == "cf") {
+        forward(2);
+        Serial.print(SUCC);
+      }
+      else if (cmd == "cb") {
+        backward(2);
+        Serial.print(SUCC);
+      }
+      else if (cmd == "cl") {
+        left(1);
+        Serial.print(SUCC);
+      }
+      else if (cmd == "cr") {
+        right(1);
+        Serial.print(SUCC);
+      }
+    }
+  }
 }
